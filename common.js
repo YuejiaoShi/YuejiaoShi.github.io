cec3ea595b726df293d05db1150685ec4966850d
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-06-10 20:12:48 +0200

    Create data folder and put my recipes data in a recipes.json file in data folder; refactor index.js and recipe-grid.js to fetch data from json file.

---
+++ common.js
@@ -0,0 +1,42 @@
+// Handle Home link and Recipes Grid link
+export function setNavBar() {
+  const setHome = document.getElementById("nav-home");
+  if (setHome) {
+    setHome.addEventListener("click", () => {
+      window.location.href = "index.html";
+    });
+  }
+
+  const gridOfAllRecipes = document.getElementById("nav-recipes");
+  if (gridOfAllRecipes) {
+    gridOfAllRecipes.addEventListener("click", () => {
+      localStorage.removeItem("filteredRecipes");
+      window.location.href = "recipe-grid.html";
+    });
+  }
+}
+
+// Handle Search bar
+export function setSearch(recipes) {
+  const searchInput = document.getElementById("search-input");
+  const searchButton = document.getElementById("search-icon");
+  searchButton.addEventListener("click", handleSearch);
+
+  function searchRecipes(inputString) {
+    inputString = inputString.trim().toLowerCase();
+    if (inputString) {
+      return recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(inputString)
+      );
+    } else {
+      return recipes;
+    }
+  }
+  function handleSearch(event) {
+    event.preventDefault();
+    const inputString = searchInput.value;
+    const filteredRecipes = searchRecipes(inputString);
+    localStorage.setItem("filteredRecipes", JSON.stringify(filteredRecipes));
+    window.location.href = `recipe-grid.html`;
+  }
+}
---
+++ recipe-grid.js
@@ -0,0 +1,93 @@
+import { setNavBar, setSearch } from "./common.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initializePage();
+
+  fetch("./data/recipes.json")
+    .then((response) => response.json())
+    .then((data) => {
+      initializePage(data.recipes);
+    })
+    .catch((error) => console.error("Error fetching the recipes:", error));
+});
+function initializePage(recipes) {
+  setNavBar();
+  setSearch(recipes);
+  setSorting(recipes);
+
+  const filteredRecipesStr = localStorage.getItem("filteredRecipes");
+  recipes = recipes || [];
+  if (filteredRecipesStr) {
+    const filteredRecipes = JSON.parse(filteredRecipesStr);
+    createRecipeGrid(filteredRecipes);
+  } else {
+    const newRecipeStr = localStorage.getItem("newRecipe");
+    if (newRecipeStr) {
+      const newRecipe = JSON.parse(newRecipeStr);
+      recipes.push(newRecipe);
+    }
+    createRecipeGrid(recipes);
+  }
+}
+
+// Make recipes grid main contents
+function createRecipeGrid(recipesToShow) {
+  const recipeGrid = document.getElementById("recipe-grid-container");
+  recipeGrid.innerHTML = "";
+
+  recipesToShow.forEach((recipe) => {
+    const recipeCard = document.createElement("div");
+    recipeCard.classList.add("recipe-card");
+
+    const recipeLink = document.createElement("a");
+    recipeLink.href = `recipe-details.html?id=${recipe.id}`;
+
+    const img = document.createElement("img");
+    img.src = recipe.picture_url;
+
+    const imgCaption = document.createElement("figcaption");
+    imgCaption.textContent = recipe.title;
+
+    recipeLink.appendChild(img);
+    recipeLink.appendChild(imgCaption);
+
+    recipeCard.appendChild(recipeLink);
+    recipeGrid.appendChild(recipeCard);
+  });
+}
+
+// Handle sort
+function sortRecipes(sortOption, recipes) {
+  if (sortOption === "by-increasing-ingredient-amount") {
+    return recipes
+      .slice()
+      .sort((a, b) => a.ingredients.length - b.ingredients.length);
+  } else if (sortOption === "by-decreasing-ingredient-amount") {
+    return recipes
+      .slice()
+      .sort((a, b) => b.ingredients.length - a.ingredients.length);
+  } // more  options in the future
+  return recipes;
+}
+
+function setSorting(recipes) {
+  const sortSelect = document.getElementById("sort-by");
+  sortSelect.addEventListener("change", handleSort);
+
+  function handleSort() {
+    const sortSelect = document.getElementById("sort-by");
+    const selectedOption = sortSelect.value;
+
+    const filteredRecipesStr = localStorage.getItem("filteredRecipes");
+    let filteredRecipes = [];
+
+    if (filteredRecipesStr) {
+      filteredRecipes = JSON.parse(filteredRecipesStr);
+    } else {
+      filteredRecipes = recipes;
+    }
+
+    const sortedRecipes = sortRecipes(selectedOption, filteredRecipes);
+    createRecipeGrid(sortedRecipes);
+  }
+}
